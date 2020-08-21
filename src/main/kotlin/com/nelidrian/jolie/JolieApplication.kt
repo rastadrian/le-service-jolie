0c4d1b7284c5cb53e6db6d65dcec834958cdbe44
Adrian Pena <rastadrian@gmail.com>
2020-08-20 22:57:39 -0400

    Adds first feature to release!

--- src/main/kotlin/com/nelidrian/jolie/JolieApplication.kt
+++ src/main/kotlin/com/nelidrian/jolie/JolieApplication.kt
@@ -1,11 +1,19 @@
 package com.nelidrian.jolie
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class JolieApplication
+class JolieApplication {
+
+	@Bean
+    fun run(): CommandLineRunner {
+        return CommandLineRunner { print("this is my first feature change!") }
+    }
+}
 
 fun main(args: Array<String>) {
-	runApplication<JolieApplication>(*args)
+    runApplication<JolieApplication>(*args)
 }
